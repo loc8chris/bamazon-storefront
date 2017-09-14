5bf1fe47d2961256d3e9885f73a841184a812650
loc8chris <loc8chris@gmail.com>
2017-09-14 00:13:27 -0500

    modified bamazonCustomer.js and added .gitingore and node modules

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -11,9 +11,67 @@ var connection = mysql.createConnection({
   database: "bamazonDB"
 });
 
+connection.connect(function(err) {
+  if (err) throw err;
+  runSearch();
+});
+
 function runSearch() {
+  var productid
+  var quantity
+
   inquirer
     .prompt({
-      name: "action",
-      type: "list",
-      message: "What would you like to do?",
+      name: "id",
+      type: "input",
+      message: "What is the ID of the product you would like to buy?"}).
+    then(function(answer){
+    	productid = answer.id
+  		inquirer
+    		.prompt({
+      			name: "quantity",
+      			type: "input",
+      			message: "How many units would you like to buy?"}).
+    			then(function(answer){
+    				quantity = answer.quantity
+    				queryDatabase(productid,quantity);
+    				console.log(answer);
+    			})
+    	console.log(answer);
+    })
+}
+function queryDatabase(productid,quantity){
+	console.log("querying database with product id " + productid + " quantity " + quantity);
+	connection.query("select * from bamazonDB.products where ?", [{item_id:productid}],
+		    function(err, res) {
+      			console.log(res);
+      			if(res[0].stock_quantity > quantity){
+					console.log("I have enough");
+					var newQuantity = res[0].stock_quantity-quantity
+					connection.query("update bamazonDB.products set ? where ?", 
+						[{stock_quantity:newQuantity},{item_id:productid}],
+						function(err, res){
+							if(err) throw err;
+							console.log(res.affectedRows + " records affected");
+						}
+						)
+				}
+				else{
+					console.log("Insufficient quantity!");
+				}
+ 
+    })
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
